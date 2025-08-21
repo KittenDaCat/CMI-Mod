1a657ee7ce59d01f755434cfb0514ba91a3738f0
KittenDaCat <kittendacat1@gmail.com>
2025-08-21 11:52:12 +1000

    test

--- src/main/java/net/kittendacat/cmimod/CMIMod.java
+++ src/main/java/net/kittendacat/cmimod/CMIMod.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 
 public class CMIMod implements ModInitializer {
 
+    //test
+
 	public static final String MOD_ID = "cmimod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
